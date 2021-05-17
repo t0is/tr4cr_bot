9b1fb011082cb6e914344e649a8d367c52562a77
t0is <tomaseisner69@gmail.com>
2021-05-17 17:12:24 +0200

    twitch api

--- index.js
+++ index.js
@@ -6,68 +6,111 @@ const slack_token = process.env.SLACK_TOKEN;
 
 const tmi = require('tmi.js');
 
-const client = new tmi.Client({
-  options: { debug: true },
-  connection: {
-    secure: true,
-    reconnect: true
-  },
-  identity: {
-    username: 'FlamantBot',
-    password: process.env.TWITCH_OAUTH_TOKEN
-  },
-  channels: ['Agraelus', 'CzechCloud', 'PimpCSGO', 'ArcadeBulls', 'Freezecz', 'Astatoro', 'dafran',
-  'Xnapycz', 'LexVeldhuis', 'Claina', 'Kokiii_', 'Patrikturi', 'STYKO', 'forsen', 'FlyGunCZ', 'Batmanova',
-  'liveoliverr', 'Artix', 'resttpowered', 'Herdyn', 'spajKK', 'bladeito', 'marty_vole', 'KuruHS',
-  'Mrtweeday', 'TenSterakdary', 'nikdohonehleda']
-  
-  //channels: ['MADMONQ', 'nikdohonehleda']
-});
+var express        = require('express');
+var session        = require('express-session');
+var passport       = require('passport');
+var OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+var request        = require('request');
+var handlebars     = require('handlebars');
 
-client.connect();
-const web = new WebClient(slack_token);
-const slack_channel_ID='C021720QLE8';
+const TWITCH_CLIENT_ID = 'a0fuj6tm5ct79clvim9816orphqkov';
+const TWITCH_SECRET    = 'kbg441fevzdvm5r04kv0psgy8nwyy4';
+const SESSION_SECRET   = 'Tr4cr_bot_t0is_hue192*';
+const CALLBACK_URL     = 'http://localhost:3000/auth/twitch/callback';
 
-client.on('message', (channel, tags, message, self) => {
-  // Ignore echoed messages.
-    //if(self || !message.startsWith('!')) {
-    if(self){
-      return;
+
+//////////////////////////////////////////////////////////// LOGIN TWITCH API //////////////////////////////////////////////////////
+var app = express();
+app.use(session({secret: SESSION_SECRET, resave: false, saveUninitialized: false}));
+app.use(express.static('public'));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Override passport profile function to get user profile from Twitch API
+OAuth2Strategy.prototype.userProfile = function(accessToken, done) {
+  var options = {
+    url: 'https://api.twitch.tv/helix/streams?user_login=czechcloud',
+    method: 'GET',
+    headers: {
+      'Client-ID': TWITCH_CLIENT_ID,
+      'Accept': 'application/vnd.twitchtv.v5+json',
+      'Authorization': 'Bearer ' + accessToken
     }
-   
-  if((    message.toLowerCase().includes("madmonq") || 
-    message.toLowerCase().includes("madmong") || 
-    message.toLowerCase().includes("madmon") || 
-    message.toLowerCase().includes("mekong")) && 
-    (tags.username.toLowerCase() !== "nightbot" && tags.username.toLowerCase() !== "streamelements")
-  )
-  
+  };
 
- // if (message.toLowerCase().includes("!dabing"))
-  {
-    var msg_output = channel + "--> " + tags.username + ": " + message;
-    //var msg_output = tags.username + ": " + message;
-    console.log(msg_output);
-    //client.say(channel, 'hueuhe');
-
-    
-    (async () => {
-
-      // Post a message to the channel, and await the result.
-      // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
-      const result = await web.chat.postMessage({
-        text: msg_output,
-        channel: slack_channel_ID
-      });
-    
-      // The result contains an identifier for the message, `ts`.
-      console.log(`Successfully send message ${result.ts} in conversation ${slack_channel_ID}`);
-    })();
-    
- 
-  }
-
+  request(options, function (error, response, body) {
+    if (response && response.statusCode == 200) {
+      done(null, JSON.parse(body));
+      console.log(JSON.parse(body));
+    } else {
+      done(JSON.parse(body));
+      console.log(JSON.parse(body));
+    }
+  });
+}
+/*
+passport.serializeUser(function(user, done) {
+    done(null, user);
 });
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+*/
+passport.use('twitch', new OAuth2Strategy({
+    authorizationURL: 'https://id.twitch.tv/oauth2/authorize',
+    tokenURL: 'https://id.twitch.tv/oauth2/token',
+    clientID: TWITCH_CLIENT_ID,
+    clientSecret: TWITCH_SECRET,
+    callbackURL: CALLBACK_URL,
+    state: true
+  },
+  function(accessToken, refreshToken, profile, done) {
+    profile.accessToken = accessToken;
+    profile.refreshToken = refreshToken;
+
+    // Securely store user profile in your DB
+    //User.findOrCreate(..., function(err, user) {
+    //  done(err, user);
+    //});
+
+    done(null, profile);
+  }
+));
+
+// Set route to start OAuth link, this is where you define scopes to request
+app.get('/auth/twitch', passport.authenticate('twitch', { scope: 'user_read' }));
+
+// Set route for OAuth redirect
+app.get('/auth/twitch/callback', passport.authenticate('twitch', { successRedirect: '/', failureRedirect: '/' }));
+
+// Define a simple template to safely generate HTML with values from user's profile
+var template = handlebars.compile(`
+<html><head><title>Twitch Auth Sample</title></head>
+<table>
+    <tr><th>Access Token</th><td>{{accessToken}}</td></tr>
+    <tr><th>Refresh Token</th><td>{{refreshToken}}</td></tr>
+    <tr><th>Display Name</th><td>{{display_name}}</td></tr>
+    <tr><th>Bio</th><td>{{bio}}</td></tr>
+    <tr><th>Image</th><td>{{logo}}</td></tr>
+</table></html>`);
+
+// If user has an authenticated session, display it, otherwise display link to authenticate
+app.get('/', function (req, res) {
+  if(req.session && req.session.passport && req.session.passport.user) {
+    res.send(template(req.session.passport.user));
+  } else {
+    res.send('<html><head><title>Twitch Auth Sample</title></head><a href="/auth/twitch"><img src="http://ttv-api.s3.amazonaws.com/assets/connect_dark.png"></a></html>');
+  }
+});
+
+app.listen(3000, function () {
+  console.log('Twitch auth sample listening on port 3000!')
+});
+
+
+
+
 
 
 
--- install.md
+++ install.md
@@ -1,3 +1,5 @@
 npm install @slack/client
 npm install tmi.js
 npm install pm2 -g
+npm install express express-session passport passport-oauth request handlebars jquery
+
