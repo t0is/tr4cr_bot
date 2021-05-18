38153e8e3774d4c159cf580608191eff3d1710fd
t0is <tomaseisner69@gmail.com>
2021-05-18 11:17:09 +0200

    test

--- index.js
+++ index.js
@@ -1,119 +1,28 @@
-
 require('dotenv').config();
+const request = require('request');
 
-const { WebClient } = require('@slack/web-api');
-const slack_token = process.env.SLACK_TOKEN;
-
-const tmi = require('tmi.js');
-
-var express        = require('express');
-var session        = require('express-session');
-var passport       = require('passport');
-var OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
-var request        = require('request');
-var handlebars     = require('handlebars');
-
-const TWITCH_CLIENT_ID = 'a0fuj6tm5ct79clvim9816orphqkov';
-const TWITCH_SECRET    = 'kbg441fevzdvm5r04kv0psgy8nwyy4';
-const SESSION_SECRET   = 'Tr4cr_bot_t0is_hue192*';
-const CALLBACK_URL     = 'http://localhost:3000/auth/twitch/callback';
-
-
-//////////////////////////////////////////////////////////// LOGIN TWITCH API //////////////////////////////////////////////////////
-var app = express();
-app.use(session({secret: SESSION_SECRET, resave: false, saveUninitialized: false}));
-app.use(express.static('public'));
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Override passport profile function to get user profile from Twitch API
-OAuth2Strategy.prototype.userProfile = function(accessToken, done) {
-  var options = {
-    url: 'https://api.twitch.tv/helix/streams?user_login=czechcloud',
-    method: 'GET',
-    headers: {
-      'Client-ID': TWITCH_CLIENT_ID,
-      'Accept': 'application/vnd.twitchtv.v5+json',
-      'Authorization': 'Bearer ' + accessToken
+const { ApiClient } = require('twitch');
+const { RefreshableAuthProvider, StaticAuthProvider } = require('twitch-auth');
+const clientId = process.env.TWITCH_CLIENT_ID;
+const accessToken = process.env.TWITCH_ACCESS_TOKEN;
+const clientSecret = process.env.TWITCH_CLIENT_SECRET;
+const refreshToken = '999999';
+const authProvider = new RefreshableAuthProvider(new StaticAuthProvider(clientId, accessToken), {
+    clientSecret,
+    refreshToken,
+    onRefresh: (token) => {
+        // do things with the new token data, e.g. save them in your database
     }
-  };
+});
+const apiClient = new ApiClient({ authProvider });
 
-  request(options, function (error, response, body) {
-    if (response && response.statusCode == 200) {
-      done(null, JSON.parse(body));
-      console.log(JSON.parse(body));
-    } else {
-      done(JSON.parse(body));
-      console.log(JSON.parse(body));
-    }
-  });
+let userName = 'herdyn'
+
+async function isStreamLive(userName) {
+	const user = await apiClient.helix.users.getUserByName(userName);
+  console.log(user);
+	if (!user) {
+		return false;
+	}
+	return await user.getStream() !== null;
 }
-/*
-passport.serializeUser(function(user, done) {
-    done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-});
-*/
-passport.use('twitch', new OAuth2Strategy({
-    authorizationURL: 'https://id.twitch.tv/oauth2/authorize',
-    tokenURL: 'https://id.twitch.tv/oauth2/token',
-    clientID: TWITCH_CLIENT_ID,
-    clientSecret: TWITCH_SECRET,
-    callbackURL: CALLBACK_URL,
-    state: true
-  },
-  function(accessToken, refreshToken, profile, done) {
-    profile.accessToken = accessToken;
-    profile.refreshToken = refreshToken;
-
-    // Securely store user profile in your DB
-    //User.findOrCreate(..., function(err, user) {
-    //  done(err, user);
-    //});
-
-    done(null, profile);
-  }
-));
-
-// Set route to start OAuth link, this is where you define scopes to request
-app.get('/auth/twitch', passport.authenticate('twitch', { scope: 'user_read' }));
-
-// Set route for OAuth redirect
-app.get('/auth/twitch/callback', passport.authenticate('twitch', { successRedirect: '/', failureRedirect: '/' }));
-
-// Define a simple template to safely generate HTML with values from user's profile
-var template = handlebars.compile(`
-<html><head><title>Twitch Auth Sample</title></head>
-<table>
-    <tr><th>Access Token</th><td>{{accessToken}}</td></tr>
-    <tr><th>Refresh Token</th><td>{{refreshToken}}</td></tr>
-    <tr><th>Display Name</th><td>{{display_name}}</td></tr>
-    <tr><th>Bio</th><td>{{bio}}</td></tr>
-    <tr><th>Image</th><td>{{logo}}</td></tr>
-</table></html>`);
-
-// If user has an authenticated session, display it, otherwise display link to authenticate
-app.get('/', function (req, res) {
-  if(req.session && req.session.passport && req.session.passport.user) {
-    res.send(template(req.session.passport.user));
-  } else {
-    res.send('<html><head><title>Twitch Auth Sample</title></head><a href="/auth/twitch"><img src="http://ttv-api.s3.amazonaws.com/assets/connect_dark.png"></a></html>');
-  }
-});
-
-app.listen(3000, function () {
-  console.log('Twitch auth sample listening on port 3000!')
-});
-
-
-
-
-
-
-
-
-
-
