6dc4c666b3990a8555ead2127628f2208bb7120c
T0iS <tomaseisner69@gmail.com>
2021-05-20 21:33:58 +0200

    Fixed twitch live updates, added Youtube updates for duklock

--- index.js
+++ index.js
@@ -43,13 +43,50 @@ const client = new tmi.Client({
   //channels: ['MADMONQ', 'nikdohonehleda']
 });
 
+const fetch = require("node-fetch");
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+//const herokuApp = process.env.HEROKU_APP || null;
+const youtubeFetchTimeout = 60000;
+
+const web = new WebClient(slack_token);
+const slack_channel_ID = 'C021720QLE8';
+const slack_online_update = 'C0225846R9B';
+
+
+///////TEST TEST TEST///////
+
+//const slack_channel_ID = 'C022500TU15';
+//const slack_online_update = 'C022500TU15';
+
+const youtubeApiKey = process.env.YOUTUBE_API_KEY;
+const youtubeApiUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video';
+//const discordApiKey = process.env.DISCORD_API_KEY;
+//const discordApiUrl = `https://discordapp.com/api/webhooks/${discordApiKey}`;
+
+const youtubeChannels = [
+    {
+        channelId: 'UCIAbC7emlDQs-dmgW4GlgnA',
+        channelUrl: 'https://www.youtube.com/channel/UCIAbC7emlDQs-dmgW4GlgnA'
+    }
+];
+
+let activeLiveStreams = new Set();
+
+
+
+
+
+
+
 
 function liveRequest(accessToken){
 
   channelsList.forEach(streamName => {
         streamName = streamName.replace('#', '');
    
-    setTimeout(() => {
+    
       const streamOptions = {
           url: 'https://api.twitch.tv/helix/streams?user_login=' + streamName.toLowerCase(),
           method: 'GET',
@@ -102,20 +139,110 @@ function liveRequest(accessToken){
         });
       
       };
-    },2000);
+    
     
 
   });
 }
 
+/////////////////////////////////////////////
+
+var dukName = 'DuklockPlus';
+async function fetchLiveStreamStatus() {
+    try {
+        // for(const youtubeChannel of youtubeChannels) {
+            const youtubeChannel = youtubeChannels[0];
+            console.log('Polling for ', JSON.stringify(youtubeChannel));
+            const url = `${youtubeApiUrl}&channelId=${youtubeChannel.channelId}&key=${youtubeApiKey}`;
+            const response = await fetch(url);
+            const myJson = await response.json();
+            
+            console.log('YouTube Response', JSON.stringify(myJson));
+            if(myJson && myJson.pageInfo && myJson.pageInfo.totalResults > 0) {
+                console.log('Found active stream for ', youtubeChannel.channelId);
+                myJson.items.forEach(element => {
+                    if(!activeLiveStreams.has(element.id.videoId)) {
+                        console.log(element);
+                        activeLiveStreams.add(element.id.videoId);
+                        if (!liveChannels.includes(dukName)){
+                            liveChannels.push(dukName);
+                            (async () => {
+                            var msg_output = "DuklockPlus is now live on YouTube.";
+                            // Post a message to the channel, and await the result.
+                            // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
+                            const result = await web.chat.postMessage({
+                                text: msg_output,
+                                channel: slack_online_update
+                            });
+                            console.log(`Successfully send message in conversation ${slack_online_update}`);
+                            })();
 
 
+                        }
+                    } else {
+                        console.log(`Already alerted for this livestream ${element.id.videoId}. Skipping.`);
+                    }
+                });
+            }
+            else {
+              console.log ('not active now');
+              liveChannels = liveChannels.filter(item => item !== dukName);
+            }
+        // }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+/*
+async function postToDiscord(json) {
+    const resp = fetch(discordApiUrl, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrer: 'no-referrer',
+        body: JSON.stringify(json)
+    }).catch(error => console.log('Discord POST failed.', JSON.stringify(json), error));
+
+    const content = await resp.json();
+    console.log('Discord response', content); 
+}
+
+async function herokuKeepAlive() {
+    try {
+        const response = await fetch(herokuApp);
+        console.log('Heroku Keep-Alive Success')
+    } catch(error) {
+        console.error(error);
+    }
+}
+*/
+//app.get('/', (req, res) => res.send('Shhh! Im busy monitoring Youtube Channels.'));
+//////////////////
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+    setInterval(fetchLiveStreamStatus, youtubeFetchTimeout);
+
+    // check jestli je live
+    setTimeout(() => {
+    request.post(options, (err,res,body)=>{
+        if(err){
+            return console.log(err);
+        }
+        
+        liveRequest(body.access_token)
+        
+      });
+    },5000);
+})
 
 
 client.connect();
-const web = new WebClient(slack_token);
-const slack_channel_ID = 'C021720QLE8';
-const slack_online_update = 'C0225846R9B';
 
 
 client.on('message', (channel, tags, message, self) => {
@@ -125,14 +252,6 @@ client.on('message', (channel, tags, message, self) => {
       return;
     }
     
-    request.post(options, (err,res,body)=>{
-      if(err){
-          return console.log(err);
-      }
-      
-      liveRequest(body.access_token)
-      
-    });
 
   if((    message.toLowerCase().includes("madmonq") || 
     message.toLowerCase().includes("madmong") || 
@@ -167,6 +286,8 @@ client.on('message', (channel, tags, message, self) => {
   }
 
 });
+
+console.log("konec, jedem async")
 
 
 
--- install.md
+++ install.md
@@ -1,3 +1,1 @@
-npm install @slack/client
-npm install tmi.js
-npm install pm2 -g
+npm install @slack/client tmi.js pm2 express express-session passport passport-oauth request handlebars jquery async node-fetch
